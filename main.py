a23252742b7a38e010d1bd168f40d1573329cd16
FirowMD <georkuchiev@mail.ru>
2023-08-11 11:41:39 +0300

    Build 2.0

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class App(tk.Tk):
         new_frame = frame_class(self)
         if self._frame is not None:
             self._frame.destroy()
-        
+
         self._frame = new_frame
         self._frame.pack()
 
@@ -25,44 +25,75 @@ class PageMain(tk.Frame):
         self.is_changing_shortcut = False
         self.shortcut = []
 
-        self.title = tk.Label(self, text="Main Page")
+        self.title = tk.Label(self, text="Main page")
         self.title.pack()
 
         self.label = tk.Label(self, text="Set shortcut")
         self.label.pack()
 
+        self.shortcut_widget = ShortcutWidget(self, self.shortcut)
+        self.shortcut_widget.pack()
+
+class ShortcutWidget(tk.Frame):
+    def __init__(self, master, shortcut):
+        tk.Frame.__init__(self, master)
+        self.service_keys = ["Control_L", "Control_R", "Alt_L", "Alt_R", "Shift_L", "Shift_R"]
+        self.is_bad_shortcut = True
+        self.is_changing_shortcut = False
+        self.old_shortcut = []
+        self.shortcut = shortcut
+
         self.text_var = tk.StringVar()
         self.text = tk.Entry(self, textvariable=self.text_var)
-        self.text.pack()
+        self.text.pack(side=tk.LEFT, fill=tk.X, expand=True)
 
         self.button = tk.Button(self, text="Change", command=self.change_shortcut)
-        self.button.pack()
+        self.button.pack(side=tk.LEFT)
 
         self.bind_all("<KeyPress>", self.shortcut_press_handler)
         self.bind_all("<KeyRelease>", self.shortcut_release_handler)
+
+    def shortcut_has_general_keys(self):
+        for key in self.shortcut:
+            if key in self.service_keys:
+                continue
+
+            return True
+            
+        return False
     
+    def shortcut_sort(self):
+        self.shortcut.sort(key=lambda x: self.service_keys.index(x) if x in self.service_keys else len(self.service_keys))
+
+    def shortcut_key_add(self, key):
+        if not key in self.shortcut:
+            self.shortcut.append(key)
+            self.shortcut_sort()
+            self.text_var.set("+".join(self.shortcut))
+
     def change_shortcut(self):
+        self.shortcut = []
         self.is_changing_shortcut = True
         self.button.configure(state = tk.DISABLED, text='Press key')
 
     def shortcut_press_handler(self, event):
         if self.is_changing_shortcut:
-            if event.keysym in ["Control_L", "Control_R", "Alt_L", "Alt_R", "Shift_L", "Shift_R"]:
-                self.shortcut.append(event.keysym)
-                self.text_var.set("+".join(self.shortcut))
-
+            self.shortcut_key_add(event.keysym)
 
     def shortcut_release_handler(self, event):
         if self.is_changing_shortcut:
-            if event.keysym in ["Control_L", "Control_R", "Alt_L", "Alt_R", "Shift_L", "Shift_R"]:
-                self.shortcut.remove(event.keysym)
+            if not self.shortcut_has_general_keys():
+                # Reset shortcut
+                self.shortcut = self.old_shortcut
                 self.text_var.set("+".join(self.shortcut))
-            else:
-                self.shortcut.append(event.keysym)
-                self.text_var.set("+".join(self.shortcut))
-                self.shortcut.clear()
-                self.button.configure(state=tk.NORMAL, text='Change')
                 self.is_changing_shortcut = False
+                self.button.configure(state = tk.NORMAL, text='Change')
+            else:
+                # Apply shortcut
+                self.old_shortcut = self.shortcut
+                self.text_var.set("+".join(self.shortcut))
+                self.is_changing_shortcut = False
+                self.button.configure(state = tk.NORMAL, text='Change')
 
 def main():
     app = App()
